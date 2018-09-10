768dc11ad7668b1b44783df011d58fcfe614ae4c
Christine Xiang <yutingcxiang@gmail.com>
2018-09-10 07:42:16 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,34 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+    
+  def artist_name=(name)
+     self.artist_name = Artist.find_or_create_by(name: name)
+   end
+ 
+   def artist_name
+      self.artist_name ? self.artist.name : nil
+   end
+   
+    def genre_name=(name)
+     self.genre_name = Genre.find_or_create_by(name: name)
+   end
+ 
+   def genre_name
+      self.genre_name ? self.genre.name : nil
+   end
+   
+  def note_contents=(ids)
+     ids.each do |id|
+       note = Note.find(id)
+       self.notes << note
+     end
+   end
+   
+   def note_contents
+     self.notes.each do |note|
+       note.content
+     end
+   end
 end
