7f9a0a634a8c3e5b0f52530cc5338d9b8afb5adf
Christine Xiang <yutingcxiang@gmail.com>
2018-09-10 08:04:51 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents=(contents)
     contents.each do |content|
       if !content.empty?
-        self.note = Note.find_or_create_by(content: content)
+        note = Note.find_or_create_by(content: content)
         self.notes << note
       end
      end
