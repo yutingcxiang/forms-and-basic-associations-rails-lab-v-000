138e892e30bf48703a4d2eb66b64da36167a11b8
Christine Xiang <yutingcxiang@gmail.com>
2018-09-10 07:53:14 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -19,8 +19,8 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
    
-  def note_contents=(ids)
-    ids.each do |id|
+  def note_contents=(contents)
+    ids.each do |content|
       note = Note.find_or_create_by(content: content)
       self.notes << note
      end
